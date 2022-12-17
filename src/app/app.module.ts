327785f1ec42d113ed3ae9fd10ac88f06cf3c748
gabe19p <g19purselley@gmail.com>
2022-12-17 22:50:04 +0900

    capstone final push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,21 @@ import { BaseLayoutComponent } from "./base-layout/base-layout.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatLabel } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-  declarations: [AppComponent, BaseLayoutComponent, AboutComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    BaseLayoutComponent,
+    AboutComponent,
+    ContactComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,6 +30,11 @@ import { ContactComponent } from './contact/contact.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/base-layout/base-layout.component.ts
+++ src/app/base-layout/base-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, Form } from "@angular/forms";
 
 @Component({
   selector: "app-base-layout",
@@ -6,9 +7,72 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./base-layout.component.scss"],
 })
 export class BaseLayoutComponent implements OnInit {
+  calculatorForm: FormGroup;
   title: string = "LOAN CALCULATOR";
+  totalInterest!: string;
+  loanTotal!: number;
+  interestRate!: number;
+  yearTotal!: number;
+  monthlyPayment!: number;
+  interestPaid!: number;
+  totalMonths!: number;
+  totalPaid!: number;
+  fixedPayment!: string;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.calculatorForm = this.fb.group({
+      loanTotal: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+        ]),
+      ],
+      interestRate: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+        ]),
+      ],
+      yearTotal: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+        ]),
+      ],
+    });
+  }
+
+  get form() {
+    return this.calculatorForm.controls;
+  }
+
+  calculate() {
+    // convert interest into decimal
+    this.interestRate = this.form.interestRate.value / 100;
+    console.log(this.interestRate);
+
+    // total interest paid
+    this.interestPaid =
+      this.form.loanTotal.value * this.interestRate * this.form.yearTotal.value;
+    // user friendly dollar format of total interest
+    this.totalInterest = `$` + this.interestPaid.toFixed(2);
+
+    // years to months conversion
+    this.totalMonths = this.form.yearTotal.value * 12;
+
+    // type control of the loan total amount
+    this.loanTotal = parseInt(this.form.loanTotal.value);
+
+    // total amount of loan cost after interest
+    this.totalPaid = this.loanTotal + this.interestPaid;
+
+    // monthly payment
+    this.monthlyPayment = this.totalPaid / this.totalMonths;
+    this.fixedPayment = `$` + this.monthlyPayment.toFixed(2);
+  }
 }
